6f27d2dd8b5893ec3da98a32069cdfb294de9b60
Phehello Semoko-Marope <ST10163295@rcconnect.edu.za>
2024-10-23 20:39:09 +0000

    Created a new migration within the appliaction for the user information thats being stored in the database.

---
+++ Migrations/20241023182354_FirstMigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhehelloSemoko_ST10163295_PROG6212_POE.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Claims",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LSurname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    LStart = table.Column<DateOnly>(type: "date", nullable: false),
+                    LEnd = table.Column<DateOnly>(type: "date", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Claims", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Claims");
+        }
+    }
+}
